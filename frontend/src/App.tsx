b98f73889ed1adb38e99ff0a35ed77cc8ecb2fd2
Aisosa Aimufua <aisosaaimufua@gmail.com>
2024-10-04 23:06:28 -0400

    Fixed issue with webpage not loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
 import SignUpPage from './SignUpPage';
+
 import './App.css';
 
 function App() {
