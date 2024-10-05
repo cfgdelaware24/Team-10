8efa20596109d506ec11ae5ee3cdf080d8d2a632
Amer Syed <artescern@gmail.com>
2024-10-05 04:00:01 -0400

    Added SignUpPage and Registration Page for both the ngo and the professional

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
-import SignUpPage from './SignUpPage';
 import OpenPositionsPage from './OpenPositionsPage';
 import NGOProfilePage from './NGOProfilePage';
 import ApplicationsPage from './ApplicationsPage';
 import ProfCreateProfilePage from './ProfCreateProfilePage';
 import ProfProfilePage from './ProfProfilePage';
-
 import './App.css';
+
+import RegistrationPageNGO from './pages/RegistrationPageNGO';
+import RegistrationPageProfessional from './pages/RegistrationPageProfessional'; 
+import SignUpPage from './pages/SignUpPage';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/signin" element={<SignUpPage />} />
+        <Route path="/register-ngo" element={<RegistrationPageNGO />} />
+        <Route path="/register-professional" element={<RegistrationPageProfessional />} />
+
+        <Route path="/" element={<SignUpPage />} /> {/* Default route */}
         <Route path="/OpenPositionsPage" element={<OpenPositionsPage />} />
         <Route path="/NGOProfilePage" element={<NGOProfilePage/>} />
         <Route path="/signup" element={<SignUpPage />} />
