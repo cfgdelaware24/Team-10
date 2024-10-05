642313106d852e3f0e6bf51b54dd0773568ecf8b
Amer Syed <artescern@gmail.com>
2024-10-05 01:38:23 -0400

    Added Sign In & Registration Page For NGO and Professionals

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import WelcomePage from './WelcomePage';
-import SignUpPage from './SignUpPage';
-import NGOProfilePage from './NGOProfilePage';
-
-import './App.css';
+import SignInPage from './pages/SignInPage'; 
+import RegistrationPageNGO from './pages/RegistrationPageNGO';
+import RegistrationPageProfessional from './pages/RegistrationPageProfessional'; 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
-          {/* <Route path="/signup" element={<SignUpPage />} /> */}
-          <Route path="/NGOProfilePage" element={<NGOProfilePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/register-ngo" element={<RegistrationPageNGO />} />
+        <Route path="/register-professional" element={<RegistrationPageProfessional />} />
+
+        <Route path="/" element={<SignInPage />} /> {/* Default route */}
       </Routes>
     </Router>
   );
